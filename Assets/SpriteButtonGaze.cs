9bfc73d7c9e8696794ab3357620cd7e8916fbd05
MarkBebrov <pro100nemo666@gmail.com>
2024-04-08 15:49:33 +0300

    zone beta 2

--- Assets/SpriteButtonGaze.cs
+++ Assets/SpriteButtonGaze.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.U2D;
+using TMPro;
+using UnityEngine.UI;
+
 namespace Vrs.Internal
 {
     public class SpriteButtonGaze : MonoBehaviour, IVrsGazeResponder
     {
+        [SerializeField] private TextMeshProUGUI textMesh;
+        [SerializeField] private Image background;
+
         private bool mGazeAt = false;
         private Coroutine changeColorCoroutine;
+        private Coroutine changeTextVisibilityCoroutine;
+        private Coroutine changeBackgroundVisibilityCoroutine;
 
         public void SetGazedAt(bool gazedAt)
         {
@@ -17,8 +25,21 @@ namespace Vrs.Internal
                 {
                     StopCoroutine(changeColorCoroutine);
                 }
+                if (changeTextVisibilityCoroutine != null)
+                {
+                    StopCoroutine(changeTextVisibilityCoroutine);
+                }
+                if (changeBackgroundVisibilityCoroutine != null)
+                {
+                    StopCoroutine(changeBackgroundVisibilityCoroutine);
+                }
+
                 Color targetColor = gazedAt ? new Color(0, 1, 0, 0.33f) : new Color(0, 0, 0, 0);
                 changeColorCoroutine = StartCoroutine(ChangeColorGradually(targetColor));
+                float targetAlphaText = gazedAt ? 1.0f : 0f;
+                changeTextVisibilityCoroutine = StartCoroutine(ChangeTextVisibilityGradually(targetAlphaText));
+                float targetAlphaBackground = gazedAt ? background.color.a : 0f;
+                changeBackgroundVisibilityCoroutine = StartCoroutine(ChangeBackgroundVisibilityGradually(targetAlphaBackground, gazedAt));
             }
         }
 
@@ -39,6 +60,41 @@ namespace Vrs.Internal
             renderer.color = targetColor;
         }
 
+        private IEnumerator ChangeTextVisibilityGradually(float targetAlpha)
+        {
+            float duration = 0.33f;
+            float elapsedTime = 0;
+            Color startColor = textMesh.color;
+            Color targetColor = new Color(startColor.r, startColor.g, startColor.b, targetAlpha);
+
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                textMesh.color = Color.Lerp(startColor, targetColor, elapsedTime / duration);
+                yield return null;
+            }
+
+            textMesh.color = targetColor;
+        }
+
+        private IEnumerator ChangeBackgroundVisibilityGradually(float targetAlpha, bool gazedAt)
+        {
+            float duration = 0.33f;
+            float elapsedTime = 0;
+            Color originalColor = background.color;
+            Color targetColor = new Color(originalColor.r, originalColor.g, originalColor.b, gazedAt ? 0.78f : 0f);
+
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                background.color = Color.Lerp(originalColor, targetColor, elapsedTime / duration);
+                yield return null;
+            }
+
+            background.color = targetColor;
+        }
+
+
         public bool isGazedAt()
         {
             return mGazeAt;
@@ -54,15 +110,8 @@ namespace Vrs.Internal
             SetGazedAt(false);
         }
 
-        public void OnGazeTrigger()
-        {
+        public void OnGazeTrigger() { }
 
-        }
-
-        public void OnUpdateIntersectionPosition(Vector3 position)
-        {
-
-        }
-
+        public void OnUpdateIntersectionPosition(Vector3 position) { }
     }
 }
