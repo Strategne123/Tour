a510cf1eba29b565744c4220f5152caa86e8443f
lfybbk10 <63192458+lfybbk10@users.noreply.github.com>
2024-04-12 09:53:17 +0300

    fix network

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadMultiplayer()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadSinglePlayer()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit() => Application.Quit();
+}
---
+++ Assets/Scripts/Network/NetworkQuestion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mirror;
+using UnityEngine;
+using Vrs.Internal;
+
+
+public class NetworkQuestion : NetworkBehaviour
+{
+    private List<SpriteButtonGaze> _spriteButtonGazes;
+
+    private void Awake()
+    {
+        _spriteButtonGazes = GetComponentsInChildren<SpriteButtonGaze>().ToList<SpriteButtonGaze>();
+    }
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < _spriteButtonGazes.Count; i++)
+        {
+            var index = i;
+            _spriteButtonGazes[i].OnGazeEntered += (() => OnGazeEnter(index));
+            _spriteButtonGazes[i].OnGazeExited += (() => OnGazeExit(index));
+        }
+        
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < _spriteButtonGazes.Count; i++)
+        {
+            var index = i;
+            _spriteButtonGazes[i].OnGazeEntered -= (() => OnGazeEnter(index));
+            _spriteButtonGazes[i].OnGazeExited -= (() => OnGazeExit(index));
+        }
+    }
+
+
+    [Command]
+    private void OnGazeEnter(int index)
+    {
+        _spriteButtonGazes[index].OnGazeEnter();
+    }
+
+    [Command]
+    private void OnGazeExit(int index)
+    {
+        _spriteButtonGazes[index].OnGazeExit();
+    }
+}
